dbc36a17974580537b2d9d90ca8f5d3dbfb4aa65
Daniel Merys <dkmerys@gmail.com>
2020-04-08 15:33:36 -0700

    include functional 'addRoll' method

--- js/scripts.js
+++ js/scripts.js
@@ -1,37 +1,46 @@
 // back-end
-function Counter(gameScore, roundScore){
+function Counter(gameScore, roundScore, grScore){
   this.gamescore = gameScore;
   this.roundScore = roundScore
+  this.grScore = game
 }
 var player1Total = new Counter(0, 0);
 var player2Total = new Counter(0, 0);
 
 
 var roll = function() {
-  console.log(Math.floor(Math.random() * 6) +1);
-  
+  var dieRoll = Math.floor(Math.random() * 6) +1;
+  return dieRoll
 }
 
 Counter.prototype.addRoll = function () {
   this.roundScore = this.roundScore + roll();
   console.log(this.roundScore)
+  
 }
 
-// Counter.prototype.hold = function {
-//   player1GameTotal.score + 
-// };
+Counter.prototype.addRound = function () {
+  this.gameScore = this.gameScore
+}
+
 
 // UI Logic
 $(document).ready(function() {
-  var player1Roll = 0;
   $("#player1-roll").click(function(){
     player1Total.addRoll()
-    $("#results").text(this.roundScore);
+    $("#results").append(player1Total.roundScore);
   });
-  $("#player2-roll").click(roll);
-  var player1Hold = $("#player1-hold").click();
+
+  $("#player1-hold").click(function(){
+    player1Total.addRound()
+  });
+
+  $("#player2-roll").click(function() {
+    player2Total.addRoll()
+  });
+
   var player2Hold = $("#player2-hold").click();
   
-  console.log("Player1Roll: " + player1Roll);
+ 
   
 });
